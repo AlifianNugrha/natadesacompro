d21505b2812758bf5d1fcfac5302cf6287da9c9b
Pandu Alifian <fmenbuilder@gmail.com>
2026-01-29 12:56:19 +0700

    update ntds

--- data/projects.ts
+++ data/projects.ts
@@ -56,7 +56,9 @@ export const allProjects: Project[] = [
         kategori: "Rencana Induk Desa",
         category: "masterplan",
         badge: "NEW",
-        content: "Berlokasi di Kutai Kartanegara, desa ini memiliki potensi ekonomi besar di sektor perkebunan kelapa sawit dan pertanian pangan, serta peluang strategis pengembangan agrowisata berbasis edukasi alam dan pemberdayaan masyarakat melalui BUMDes."
+        content: "Berlokasi di Kutai Kartanegara, desa ini memiliki potensi ekonomi besar di sektor perkebunan kelapa sawit dan pertanian pangan, serta peluang strategis pengembangan agrowisata berbasis edukasi alam dan pemberdayaan masyarakat melalui BUMDes.",
+        gallery: ["/porto/loalepu/loalepu.jpg", "/porto/loalepu/loalepu.jpg", "/porto/loalepu/loalepu.jpg"]
+
     },
     {
         id: "desa-muara-muntai-ilir",
@@ -68,7 +70,9 @@ export const allProjects: Project[] = [
         kategori: "Rencana Induk Desa",
         category: "masterplan",
         badge: "NEW",
-        content: "Penyusunan rencana induk desa yang fokus pada optimalisasi potensi perairan pedalaman dan pengembangan pariwisata sungai sebagai penggerak ekonomi berbasis lingkungan."
+        content: "Penyusunan rencana induk desa yang fokus pada optimalisasi potensi perairan pedalaman dan pengembangan pariwisata sungai sebagai penggerak ekonomi berbasis lingkungan.",
+        gallery: ["/porto/muarailir/muarailir.jpeg", "/porto/muarailir/muarailir.jpeg", "/porto/muarailir/muarailir.jpeg"]
+
     },
     {
         id: "desa-bumi-etam",
@@ -80,21 +84,89 @@ export const allProjects: Project[] = [
         kategori: "Rencana Induk Desa",
         category: "masterplan",
         badge: "NEW",
-        content: "Perencanaan strategis yang diarahkan untuk menjadikan Desa Bumi Etam sebagai pusat pertumbuhan ekonomi baru di Kutai Timur melalui penguatan infrastruktur dan manajemen sumber daya desa."
+        content: "Perencanaan strategis yang diarahkan untuk menjadikan Desa Bumi Etam sebagai pusat pertumbuhan ekonomi baru di Kutai Timur melalui penguatan infrastruktur dan manajemen sumber daya desa.",
+        gallery: ["/porto/bumietan/bumietan.jpeg", "/porto/bumietan/bumietan.jpeg", "/porto/bumietan/bumietan.jpeg"]
+
     },
+
     {
-        id: "RW-01-Desa-Ponggok",
+        id: "desa-ghatak",
         year: "2025",
-        name: "RW 01 Desa Ponggok Fish and Farm",
+        name: "Desa Ghatak",
         kecamatan: "Kaubun",
-        kabupaten: "Kutai Timur",
+        kabupaten: "Kutai Kartanegara",
         provinsi: "Kalimantan Timur",
         kategori: "Rencana Induk Desa",
         category: "masterplan",
         badge: "NEW",
-        content: "Perencanaan strategis yang diarahkan untuk menjadikan Desa Bumi Etam sebagai pusat pertumbuhan ekonomi baru di Kutai Timur melalui penguatan infrastruktur dan manajemen sumber daya desa."
+        content: "Perencanaan strategis yang diarahkan untuk menjadikan Desa Ghatak sebagai pusat pertumbuhan ekonomi baru di Kutai Timur melalui penguatan infrastruktur dan manajemen sumber daya desa.",
+        gallery: ["/porto/ghatakatas/ghatakatas.jpg", "/porto/ghatakatas/ghatakatas.jpg", "/porto/ghatakatas/ghatakatas.jpg"]
     },
-
+    {
+        id: "pendampingan-delanggu-ecopark",
+        year: "2025",
+        name: "Pendampingan Delanggu Eco Park",
+        kecamatan: "Delanggu",
+        kabupaten: "Kutai Kartanegara",
+        provinsi: "Kalimantan Timur",
+        kategori: "Pendampingan Eco Park",
+        category: "masterplan",
+        badge: "NEW",
+        content: "Perencanaan strategis yang diarahkan untuk menjadikan Desa Ghatak sebagai pusat pertumbuhan ekonomi baru di Kutai Timur melalui penguatan infrastruktur dan manajemen sumber daya desa.",
+        gallery: ["/porto/pendampingandelanggupark/delanggu1.png", "/porto/pendampingandelanggupark/delanggu2.png", "/porto/pendampingandelanggupark/delanggu2.png"]
+    },
+    {
+        id: "RFID-Umbul-Ponggok",
+        year: "2025",
+        name: "RFID Umbul Ponggok",
+        kecamatan: "Delanggu",
+        kabupaten: "Kutai Kartanegara",
+        provinsi: "Kalimantan Timur",
+        kategori: "RFID-Umbul-Ponggok",
+        category: "masterplan",
+        badge: "NEW",
+        content: "Perencanaan strategis yang diarahkan untuk menjadikan Desa Ghatak sebagai pusat pertumbuhan ekonomi baru di Kutai Timur melalui penguatan infrastruktur dan manajemen sumber daya desa.",
+        gallery: ["/porto/rfidumbul/rfid.png", "/porto/rfidumbul/rfid.png", "/porto/rfidumbul/rfid.png"]
+    },
+    {
+        id: "website-bumdes-tirtamandiri",
+        year: "2025",
+        name: "Website BUMDes Tirtamandiri",
+        kecamatan: "Delanggu",
+        kabupaten: "Kutai Kartanegara",
+        provinsi: "Kalimantan Timur",
+        kategori: "Website BUMDes Tirtamandiri",
+        category: "masterplan",
+        badge: "NEW",
+        content: "Perencanaan strategis yang diarahkan untuk menjadikan Desa Ghatak sebagai pusat pertumbuhan ekonomi baru di Kutai Timur melalui penguatan infrastruktur dan manajemen sumber daya desa.",
+        gallery: ["/porto/websitetirtamandiri/tirtamandiri.png", "/porto/websitetirtamandiri/tirtamandiri1.png", "/porto/websitetirtamandiri/tirtamandiri2.png"]
+    },
+    {
+        id: "bp-soko-alas",
+        year: "2025",
+        name: "BP Soko Alas",
+        kecamatan: "Delanggu",
+        kabupaten: "Kutai Kartanegara",
+        provinsi: "Kalimantan Timur",
+        kategori: "BP Soko Alas",
+        category: "masterplan",
+        badge: "NEW",
+        content: "Perencanaan strategis yang diarahkan untuk menjadikan Desa Ghatak sebagai pusat pertumbuhan ekonomi baru di Kutai Timur melalui penguatan infrastruktur dan manajemen sumber daya desa.",
+        gallery: ["/porto/sokoalas/sokoalas.png", "/porto/sokoalas/sokoalas1.png", "/porto/sokoalas/sokoalas2.png"]
+    },
+    {
+        id: "bumdes-sidowayah",
+        year: "2025",
+        name: "BUMDes Sidowayah",
+        kecamatan: "Delanggu",
+        kabupaten: "Kutai Kartanegara",
+        provinsi: "Kalimantan Timur",
+        kategori: "BUMDes Sidowayah",
+        category: "masterplan",
+        badge: "NEW",
+        content: "Perencanaan strategis yang diarahkan untuk menjadikan Desa Ghatak sebagai pusat pertumbuhan ekonomi baru di Kutai Timur melalui penguatan infrastruktur dan manajemen sumber daya desa.",
+        gallery: ["/porto/bumdessidowayah/bumdessidowayah.png", "/porto/bumdessidowayah/bumdessidowayah1.png", "/porto/bumdessidowayah/bumdessidowayah2.png"]
+    },
 
 
 ];

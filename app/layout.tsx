f1162765f0ce3c9cffa88cd79567e2435ca33991
Pandu Alifian <fmenbuilder@gmail.com>
2026-01-29 14:57:49 +0700

    update ntda

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
 
   // VERIFIKASI GOOGLE SEARCH CONSOLE (Ganti kode di bawah ini)
   verification: {
-    google: "google662673fc127acaa3.html",
+    google: "google662673fc127acaa3",
   },
 
   alternates: {
